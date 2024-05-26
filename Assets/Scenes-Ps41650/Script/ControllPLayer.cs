83419428f2f8bcc629a27550af2cccea927bcde0
HakaiPham <169151162+HakaiPham@users.noreply.github.com>
2024-05-26 13:00:05 +0700

    Scene1_Updatecode 2

--- Assets/Scenes-Ps41650/Script/ControllPLayer.cs
+++ Assets/Scenes-Ps41650/Script/ControllPLayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllPlayer: MonoBehaviour
+public class ControllPLayer : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private float _speed;
