bcb30c62fb1bce88512af91295d6d0a5d1d371b2
HakaiPham <169151162+HakaiPham@users.noreply.github.com>
2024-06-07 10:56:09 +0700

    Commit

--- Assets/resource/Script/UiManager.cs
+++ Assets/resource/Script/UiManager.cs
@@ -16,11 +16,7 @@ public class UiManager : MonoBehaviour
     Memory _memoryGame;
     void Start()
     {
-<<<<<<< HEAD
-=======
-        //_PanelAchivement.SetActive(false);
-        //_PanelHelp.SetActive(false);
->>>>>>> main
+
     }
 
     // Update is called once per frame
